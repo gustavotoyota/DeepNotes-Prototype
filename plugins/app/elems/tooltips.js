9eb3d6dda66e6185c2c44afabc6655a239807803
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-12-29 18:45:39 -0400

    Deactivated tooltip fix

--- plugins/app/elems/tooltips.js
+++ plugins/app/elems/tooltips.js
@@ -4,13 +4,13 @@ const tooltips = module.exports = {}
 
 
 tooltips.fix = () => {
-  if (!$getters.activeElem)
-    return
+  // if (!$getters.activeElem)
+  //   return
 
-  const tooltips = document.querySelectorAll(`#elem-${$getters.activeElem.id} .ql-tooltip`)
-  if (tooltips.length === 0)
-    return
+  // const tooltips = document.querySelectorAll(`#elem-${$getters.activeElem.id} .ql-tooltip`)
+  // if (tooltips.length === 0)
+  //   return
 
-  for (const tooltip of tooltips)
-    tooltip.style.transform = `scale(${1 / $getters.currentPage.camera.zoom})`
+  // for (const tooltip of tooltips)
+  //   tooltip.style.transform = `scale(${1 / $getters.currentPage.camera.zoom})`
 }
