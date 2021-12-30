25a66f879f7615112ecfb9fa6f82155761aa971e
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2021-12-29 21:53:44 -0400

    Fixed Quill ghost tooltip bug

--- plugins/app/elems/editing.js
+++ plugins/app/elems/editing.js
@@ -22,6 +22,7 @@ editing.start = (elem, editorIdx) => {
     const quill = quillEditors[editorIdx ?? 0].__vue__.quill
 
     quill.focus()
+    quill.setSelection(0, 0)
     quill.setSelection(0, Infinity, 'user')
   })
 }
@@ -29,22 +30,5 @@ editing.start = (elem, editorIdx) => {
 
 
 editing.stop = () => {
-  const page = $getters.currentPage
-
-  if (!page.elems.editing)
-    return
-  
-  page.elems.editing = false
-  
-  const elem = $app.elems.getById(page.elems.activeId)
-  if (!elem)
-    return
-
-  const quillEditors = document.querySelectorAll(`#elem-${elem.id} .quill-editor`)
-
-  for (const quillEditor of quillEditors) {
-    const quill = quillEditor.__vue__.quill
-
-    quill.setSelection(0, 0, 'user')
-  }
+  $getters.currentPage.elems.editing = false
 }
