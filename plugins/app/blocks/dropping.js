0c358598d42d480a9e64bf3867e93be088aa7b7a
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2022-01-07 21:23:20 -0400

    Add scrollIntoView on dropping

--- plugins/app/blocks/dropping.js
+++ plugins/app/blocks/dropping.js
@@ -7,4 +7,9 @@ dropping.perform = (event, regionElem, dropIndex) => {
   $app.selection.moveToRegion(regionElem, dropIndex)
   
   $app.dragging.finish(event)
+
+  $nextTick(() => {
+    const lastSelectedElem = $app.selection.getElems().at(-1)
+    $app.elems.scrollIntoView(lastSelectedElem)
+  })
 }
