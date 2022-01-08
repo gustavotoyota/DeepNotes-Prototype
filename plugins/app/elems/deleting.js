578dbf3896526ee7f67c6afe4bb67db72a64fc4d
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2022-01-08 14:14:51 -0400

    Improve deleting.deleteSelection

--- plugins/app/elems/deleting.js
+++ plugins/app/elems/deleting.js
@@ -3,11 +3,9 @@ const deleting = module.exports = {}
 
 
 
-deleting.deleteElem = (elem) => {
-  $app.selection.remove(elem)
-  $app.elems.removeFromRegion(elem)
-}
 deleting.deleteSelection = () => {
   for (const elem of $getters.elems)
-    $app.deleting.deleteElem(elem)
+    $app.elems.removeFromRegion(elem)
+
+  $app.selection.clear()
 }
