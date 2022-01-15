cd3382914d169d4884d1dd535be5fcb334997f57
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2022-01-15 17:09:08 -0400

    Make notes centered on creation

--- plugins/app/blocks/notes.js
+++ plugins/app/blocks/notes.js
@@ -3,7 +3,7 @@ const notes = module.exports = {}
 
 
 
-notes.create = (clientPos) => {
+notes.create = async (clientPos) => {
   const elem = $app.blocks.create({
     type: 'block',
 
@@ -15,6 +15,26 @@ notes.create = (clientPos) => {
   $app.defaultProps.copy([elem])
 
   $app.editing.start(elem)
+
+
+
+
+  // Make notes centered on creation
+
+  await $nextTick()
+
+  const clientRect = $app.elems.getClientRect(elem)
+
+  const worldSize = {
+    x: $app.sizes.screenToWorld1D(clientRect.width),
+    y: $app.sizes.screenToWorld1D(clientRect.height),
+  }
+
+  elem.pos.x += worldSize.x * (elem.anchor.x - 0.5)
+  elem.pos.y += worldSize.y * (elem.anchor.y - 0.5)
+
+
+  
 
   return elem
 }
